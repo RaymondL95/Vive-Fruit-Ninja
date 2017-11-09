5e26f4f92bf6542f526de26633df0ca355e8b506
RaymondL95 <Raymondleung95@gmail.com>
2017-11-09 18:11:00 -0500

    added developer keys

---
+++ ViveButtonFruitScene/Assets/Scripts/Developer Inputs/Inputs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Inputs : MonoBehaviour {
+    [SerializeField] private SwordCutter sword;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("x"))
+        {
+            sword.slider.value = 1f;
+        }
+	}
+}
