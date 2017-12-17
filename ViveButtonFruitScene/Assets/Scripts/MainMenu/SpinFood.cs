38c8f1baea7f60d31b0c7abf0a673a0ed5667425
RaymondL95 <raymondleung95@gmail.com>
2017-12-17 11:08:26 -0500

    Settings Page completed and started score page

---
+++ ViveButtonFruitScene/Assets/Scripts/MainMenu/SpinFood.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinFood : MonoBehaviour {
+    public float offset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.right * Time.deltaTime * offset);
+    }
+}
