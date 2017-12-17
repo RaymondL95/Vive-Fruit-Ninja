9d92f0facf869457a15457e1f5d5c43daab89ba5
RaymondL95 <raymondleung95@gmail.com>
2017-12-17 10:47:41 -0500

    Changed Menu

---
+++ ViveButtonFruitScene/Assets/Scripts/MainMenu/CollisionScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CollisionScene : MonoBehaviour {
+
+    // Use this for initialization
+    public string scene;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
